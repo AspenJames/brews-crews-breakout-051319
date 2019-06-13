5b36522652aebdc29d027548afa3699741b3bafe
aspenjames <aspenjames@tqca.org>
2019-06-13 15:30:59 -0700

    show page for Brew, init new action

--- app/controllers/brews_controller.rb
+++ app/controllers/brews_controller.rb
@@ -1,7 +1,21 @@
 class BrewsController < ApplicationController
+	before_action :set_brew, :only => [:show, :edit, :update]
+	# before_action :set_brew, :only => %i(show edit update)
 
 	def index
 		@brews = Brew.all
+	end
+
+	def show
+	end
+
+	def new
+		@brew = Brew.new
+	end
+
+	private
+	def set_brew
+		@brew = Brew.find(params[:id])
 	end
 
 end
